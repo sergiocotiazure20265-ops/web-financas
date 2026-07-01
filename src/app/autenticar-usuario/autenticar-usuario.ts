b316c4d411fb360ed80693cb9573ffd32d886c71
sergiocotiazure20265-ops <sergiocotiazure20265@gmail.com>
2026-07-01 12:58:45 -0300

    Integração da página de cadastro de usuário, validação dos formulários e autenticação.

--- src/app/autenticar-usuario/autenticar-usuario.ts
+++ src/app/autenticar-usuario/autenticar-usuario.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -17,6 +17,9 @@ export class AutenticarUsuario {
   //e incializa-lo por injeção de dependência
   private http = inject(HttpClient);
 
+  //Exibição de mensagens
+  mensagemErro = signal<string>('');
+
   //Objeto para capturar o formulário de autenticação
   formAutenticar = new FormGroup({
     email : new FormControl('', [Validators.required]),
@@ -29,11 +32,14 @@ export class AutenticarUsuario {
     //Enviando uma requisição HTTP POST para a API
     this.http.post('http://localhost:8082/api/v1/usuario/autenticar', this.formAutenticar.value)
       .subscribe({
-        next: (response) => {
-          console.log('Sucesso!', response);
+        next: (response: any) => {
+          //Salvar os dados do usuário autenticado na sessão do navegador
+          sessionStorage.setItem('auth', JSON.stringify(response));
+          //Redirecionar para o dashboard do sistema
+          location.href = '/app/dashboard';
         },
         error: (e) => {
-          console.log('Erro!', e.error);
+          this.mensagemErro.set(e.error);
         }
       });
   }
--- src/app/criar-usuario/criar-usuario.ts
+++ src/app/criar-usuario/criar-usuario.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-usuario',
@@ -18,6 +18,10 @@ export class CriarUsuario {
   //Injeção de dependência da classe HttpClient
   private http = inject(HttpClient);
 
+  //Atributos (signal)
+  mensagemSucesso = signal<string>('');
+  mensagemErro = signal<string>('');
+
   //Criando a estrutura do formulário
   formCriarUsuario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(8)]),
@@ -25,10 +29,33 @@ export class CriarUsuario {
     senha: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/)]),
     senhaConfirmacao: new FormControl('', [Validators.required]),
     aceiteTermos: new FormControl(false, [Validators.requiredTrue])
+  }, {
+    validators: this.validarSenhasIguais
   });
+
+  //Função para validação customizada do campo 'senhaConfirmacao'
+  validarSenhasIguais(control: AbstractControl) : ValidationErrors | null {
+
+    //capturar o valor preenchido nos campos 'senha' e 'senhaConfirmacao'
+    const valorSenha = control.get('senha')?.value;
+    const valorSenhaConfirmacao = control.get('senhaConfirmacao')?.value;
+
+    //verificar se o usuário informou a senha e os valores estão diferentes
+    if(valorSenha && (valorSenhaConfirmacao !== valorSenha)) {
+      //Retornar um erro de validação
+      return { senhasDiferentes: true };
+    }
+
+    return null; //Não há erros de validação
+  }
+
 
   //Função para capturar o evento de submit do formulário
   criarUsuario() {
+
+    //Definir as mensagens
+    this.mensagemSucesso.set('');
+    this.mensagemErro.set('');
 
     //Criando um JSON somente com os campos requeridos pela API
     const json = {
@@ -40,11 +67,12 @@ export class CriarUsuario {
     //Enviando a requisição para o backend
     this.http.post('http://localhost:8082/api/v1/usuario/criar', json)
       .subscribe({ //Aguardando o retorno da API
-        next: (response) => { //Capturando se o retorno for sucesso da API
-          console.log('Sucesso!', response);
+        next: (response: any) => { //Capturando se o retorno for sucesso da API
+          this.mensagemSucesso.set(`Parabéns ${response.nomeUsuario}, sua conta foi criada com sucesso!`);
+          this.formCriarUsuario.reset(); //limpar o formulário
         },
         error: (e) => { //Capturando se o retorno for erro da API
-          console.log('Erro!', e.error);
+          this.mensagemErro.set(e.error);
         }
       });
   }
